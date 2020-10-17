5241e51bec0d3e6d668dc502dd29168bd57af4c8
Administrator <1327070400@qq.com>
2020-10-17 21:11:29 +0800

    增加数据库配置，访问数据库

--- system/src/main/java/com/course/system/controller/TestController.java
+++ system/src/main/java/com/course/system/controller/TestController.java
@@ -1,13 +1,21 @@
 package com.course.system.controller;
 
+import com.course.system.domain.Test;
+import com.course.system.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class TestController {
 
+    @Autowired
+    private TestService testService;
+
     @RequestMapping("test")
-    public String test(){
-        return "success";
+    public List<Test> test(){
+        return testService.list();
     }
 }
