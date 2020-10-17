01c04368c69ba34d1d1b932faaac5d05a78d781f
Administrator <1327070400@qq.com>
2020-10-17 22:35:42 +0800

    1、添加热部署 2、优化日志输出

---
+++ doc/db/all.sql
@@ -0,0 +1,8 @@
+drop table if exists test;
+CREATE TABLE `test` (
+                        `id` varchar(255) DEFAULT NULL,
+                        `name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `course`.`test` (`id`, `name`) VALUES ('1', 'rr');
+
