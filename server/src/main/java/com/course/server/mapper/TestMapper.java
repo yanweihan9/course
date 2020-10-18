4c69e94122f5ea10a565865587f01ecb6a152d27
Administrator <1327070400@qq.com>
2020-10-18 20:44:42 +0800

    1、集成mybatis-generator 2、添加generatorConfig.xml 3、创建启动命令mybatis-generator:generate -e

--- server/src/main/java/com/course/server/mapper/TestMapper.java
+++ server/src/main/java/com/course/server/mapper/TestMapper.java
@@ -1,10 +1,22 @@
 package com.course.server.mapper;
 
 import com.course.server.domain.Test;
-
+import com.course.server.domain.TestExample;
 import java.util.List;
+import org.apache.ibatis.annotations.Param;
 
 public interface TestMapper {
+    long countByExample(TestExample example);
 
-    List<Test> list();
+    int deleteByExample(TestExample example);
+
+    int insert(Test record);
+
+    int insertSelective(Test record);
+
+    List<Test> selectByExample(TestExample example);
+
+    int updateByExampleSelective(@Param("record") Test record, @Param("example") TestExample example);
+
+    int updateByExample(@Param("record") Test record, @Param("example") TestExample example);
 }
