6c0a2ab7fe4755e991565f47862bcc6c2c5ba317
yanweihan <1327070400@qq.com>
2020-10-30 15:48:23 +0800

    1、pagination.vue还没有写

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -1,11 +1,12 @@
 <template>
     <div>
     <p>
-        <button v-on:click="list()" class="btn btn-white btn-default btn-round">
+        <button v-on:click="list(1)" class="btn btn-white btn-default btn-round">
             <i class="ace-icon fa fa-refresh red2"></i>
             刷新
         </button>
     </p>
+    <pagination ref="pagination" v-bind:list="list"></pagination>
     <table id="simple-table" class="table  table-bordered table-hover">
         <Title></Title>
         <tbody>
@@ -74,6 +75,7 @@
 </template>
 <script>
     import Title from '../admin/title';
+    import Pagination from '../../components/pagination';
 
     export default {
         name: 'chapter',
@@ -82,22 +84,24 @@
                 chapters: []
             }
         },
-        components: {Title},
+        components: {Title,Pagination},
         mounted: function () {
             // 页面激活方式一
             // this.$parent.activeSidebar("business-chapter-sidebar")
             let _this = this;
-            _this.list();
+            _this.$refs.pagination.size = 5;
+            _this.list(1);
         },
         methods: {
-            list() {
+            list(page) {
                 let _this = this;
                 _this.$ajax.post('http://127.0.0.1:9000/business/admin/chapter/list', {
-                    page: 1,
-                    size: 1
+                    page: page,
+                    size: _this.$refs.pagination.size,
                 }).then((response) => {
                     console.log("查询大章列表结果:", response);
                     _this.chapters = response.data.list;
+                    _this.$refs.pagination.render(page, response.data.total);
                 })
             }
         }
--- doc/db/all.sql
+++ doc/db/all.sql
@@ -15,7 +15,22 @@ CREATE TABLE `chapter` (
 primary key (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 comment '大章';
 
-INSERT INTO `course`.`chapter` (`id`,course_id, `name`) VALUES ('000','000', '测试大章节1');
-INSERT INTO `course`.`chapter` (`id`,course_id, `name`) VALUES ('001','002', '测试大章节2');
+INSERT INTO `course`.`chapter` (`id`,course_id, `name`) VALUES ('001','000', '测试大章节01');
+INSERT INTO `course`.`chapter` (`id`,course_id, `name`) VALUES ('002','000', '测试大章节02');
+INSERT INTO `course`.`chapter` (`id`,course_id, `name`) VALUES ('003','000', '测试大章节03');
+INSERT INTO `course`.`chapter` (`id`,course_id, `name`) VALUES ('004','000', '测试大章节04');
+INSERT INTO `course`.`chapter` (`id`,course_id, `name`) VALUES ('005','000', '测试大章节05');
+INSERT INTO `course`.`chapter` (`id`,course_id, `name`) VALUES ('006','000', '测试大章节06');
+INSERT INTO `course`.`chapter` (`id`,course_id, `name`) VALUES ('007','000', '测试大章节07');
+INSERT INTO `course`.`chapter` (`id`,course_id, `name`) VALUES ('008','000', '测试大章节08');
+INSERT INTO `course`.`chapter` (`id`,course_id, `name`) VALUES ('009','000', '测试大章节09');
+INSERT INTO `course`.`chapter` (`id`,course_id, `name`) VALUES ('010','000', '测试大章节10');
+INSERT INTO `course`.`chapter` (`id`,course_id, `name`) VALUES ('011','000', '测试大章节11');
+INSERT INTO `course`.`chapter` (`id`,course_id, `name`) VALUES ('012','000', '测试大章节12');
+INSERT INTO `course`.`chapter` (`id`,course_id, `name`) VALUES ('013','000', '测试大章节13');
+INSERT INTO `course`.`chapter` (`id`,course_id, `name`) VALUES ('014','000', '测试大章节14');
+INSERT INTO `course`.`chapter` (`id`,course_id, `name`) VALUES ('015','000', '测试大章节15');
+INSERT INTO `course`.`chapter` (`id`,course_id, `name`) VALUES ('016','000', '测试大章节16');
+
 
 
