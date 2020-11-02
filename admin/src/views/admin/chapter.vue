cc1759ab2db082d493a3e2ba9d3daf6d9e46721f
yanweihan <1327070400@qq.com>
2020-11-02 14:39:15 +0800

    1、前端增加非空和长度校验

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -114,7 +114,13 @@
 
             save() {
                 let _this = this;
-                Loading.show();
+                if (!Validator.require(_this.chapter.name, "名称")
+                    || !Validator.require(_this.chapter.courseId, "课程id")
+                    || !Validator.length(_this.chapter.courseId, "课程id", 1, 8)
+                ){
+                    return;
+                }
+                    Loading.show();
                 _this.$ajax.post('http://127.0.0.1:9000/business/admin/chapter/save',
                     _this.chapter).then((response) => {
                     Loading.hide();
