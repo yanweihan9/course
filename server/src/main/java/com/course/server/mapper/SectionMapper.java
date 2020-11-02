f1f3e21f41d0894ac18974d67a2cfa10615bcb6e
yanweihan <1327070400@qq.com>
2020-11-02 17:58:57 +0800

    1、加入了代码生成简易模板 2、生成了代码

---
+++ server/src/main/java/com/course/server/mapper/SectionMapper.java
@@ -0,0 +1,31 @@
+package com.course.server.mapper;
+
+import com.course.server.domain.Section;
+import com.course.server.domain.SectionExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface SectionMapper {
+    long countByExample(SectionExample example);
+
+    int deleteByExample(SectionExample example);
+
+    int deleteByPrimaryKey(String id);
+
+    int insert(Section record);
+
+    int insertSelective(Section record);
+
+    List<Section> selectByExample(SectionExample example);
+
+    Section selectByPrimaryKey(String id);
+
+    int updateByExampleSelective(@Param("record") Section record, @Param("example") SectionExample example);
+
+    int updateByExample(@Param("record") Section record, @Param("example") SectionExample example);
+
+    int updateByPrimaryKeySelective(Section record);
+
+    int updateByPrimaryKey(Section record);
+}
