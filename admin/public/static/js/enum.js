c618fff1a953536eb374f0916d05432a6c29bc5b
yanweihan <1327070400@qq.com>
2020-11-04 16:38:46 +0800

    1、增加前端全局枚举

---
+++ admin/public/static/js/enum.js
@@ -0,0 +1,1 @@
+CHARGE = [{key: "C", value: "收费1"}, {key: "F", value: "免费2"}];
--- admin/src/views/admin/section.vue
+++ admin/src/views/admin/section.vue
@@ -132,7 +132,7 @@
             return {
                 section: {},
                 sections: [],
-                CHARGE: [{key: "C", value: "收费"}, {key: "F", value: "免费"}]
+                CHARGE: CHARGE,
             }
         },
         components: {Title, Pagination},
